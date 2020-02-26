d1028fb1347e56e3f1474d313e2e716954e16c09
Parida <AParida@ad.harman.com>
2020-02-26 16:34:03 +0530

    sop added for remote

--- src/main/java/com/harman/SpringbootDemo3/SpringbootDemo3Application.java
+++ src/main/java/com/harman/SpringbootDemo3/SpringbootDemo3Application.java
@@ -9,6 +9,7 @@ public class SpringbootDemo3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemo3Application.class, args);
 		System.out.println("inside spdemo3...");
+		System.out.println("Added sop for modification...");
 	}
 
 }
