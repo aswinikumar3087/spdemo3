b68b3dba3ff5452c48f2d145c2fda70ece3c6e43
Parida <AParida@ad.harman.com>
2020-02-26 15:54:40 +0530

    added project to git local repo

---
+++ src/main/java/com/harman/SpringbootDemo3/controller/PropertyLoder.java
@@ -0,0 +1,22 @@
+package com.harman.SpringbootDemo3.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PropertyLoder {
+
+    @Value("${list_of_name}")
+    public String value;
+    @GetMapping("/getName")
+    public String getName(){
+    return value;
+    }
+
+
+
+
+
+}
