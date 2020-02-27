a006c434c130ca52f9b79ef88d625aa980ffe3a8
Parida <AParida@ad.harman.com>
2020-02-27 15:13:31 +0530

    added a hellomethod in HelloController

--- src/main/java/com/harman/SpringbootDemo3/controller/HelloController.java
+++ src/main/java/com/harman/SpringbootDemo3/controller/HelloController.java
@@ -1,4 +1,10 @@
 package com.harman.SpringbootDemo3.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HelloController {
+    @GetMapping("/sayHello")
+    public String getMsg(){
+        return "Hello";
+    }
 }
